dd8c68b2d4ad32be64ffd5ce7325b0877abd7ae7
rii722 <35479496+rii722@users.noreply.github.com>
2018-03-04 16:43:08 +0900

    Update main.rb

--- app/main.rb
+++ app/main.rb
@@ -5,10 +5,10 @@ def main(argv)
   # This is a sample code to use arguments and outputs.
   # Edit and remove this code as you like.
 
-#   argv.each_index do |i|
-#     v = argv[i]
-#     puts "argv[#{i}]: #{v}"
+  argv.each_index do |i|
+    v = argv[i]
+    puts "argv[#{i}]: #{v}"
 #   end
   
-  puts "Hello #{text}"
+#   puts "Hello #{text}"
 end
