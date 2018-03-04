7f06c856207b8c1396ecaac9617069d0c757108b
rii722 <35479496+rii722@users.noreply.github.com>
2018-03-04 16:36:49 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -2,19 +2,5 @@ require './app/main'
 
 main(ARGV)
 
-text = ""
-puts "HEllo #{text}!"
-
-"use strict";
-
-const expect = require("chai").expect;
-const codecheck = require("codecheck");
-const app = codecheck.consoleApp(process.env.APP_COMMAND);
-
-describe("CLI App", () => {
-  it("outputs 'Hello #{text}!' when given '#{text}' as an argument", () => {
-  	return app.codecheck("#{text}").then( result =>  {
-      expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
-      expect(result.stdout.join("")).to.equal("Hello World!");
-  	})
-  });
+text = gets
+puts "Hello #{text}"
