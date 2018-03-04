b394428e3ceacbb8d8bc9c4d300baea4c75b1d3e
rii722 <35479496+rii722@users.noreply.github.com>
2018-03-04 17:18:58 +0900

    Update index.rb

--- index.rb
+++ index.rb
@@ -12,21 +12,21 @@ describe("CLI App", () => {
 #   	return app.codecheck("World").then( result =>  {
 #       expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
 #       expect(result.stdout.join("")).to.equal("Hello World!");
-#   	})
+  	})
   });
 
   it("outputs 'Hello codecheck!' when given 'codecheck' as an argument.", () => {
 #   	return app.codecheck("codecheck").then( result =>  {
 #       expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
 #       expect(result.stdout.join("")).to.equal("Hello codecheck!");
-#   	})
+  	})
   });
 
   it("outputs 'Hello 織田信長!' when given '織田信長' as an argument.", () => {
 #   	return app.codecheck("織田信長").then( result =>  {
 #       expect(result.code).to.equal(0, "expected codecheck CLI to exit with status code 0");
 #       expect(result.stdout.join("")).to.equal("Hello 織田信長!");
-#   	})
+  	})
   });
 
 });
